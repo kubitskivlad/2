5e60e92af59844a36a5446c48971214907b7aaf5
Kubitski Vlad <Kubitskivlad@if-you-see-this-then-your-dns-resolver-is-wrong.telecom.by>
2015-09-08 22:34:56 +0300

    Initial Commit

---
+++ 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2
+//
+//  Created by Kubitski Vlad on 08.09.15.
+//  Copyright (c) 2015 Kubitski Vlad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
